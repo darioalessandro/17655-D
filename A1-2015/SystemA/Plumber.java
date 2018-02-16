634a03869e6b25d446e9c509b88ef5779a7e08f1
Dario A Lencina Talarico <dario.talarico@getcruise.com>
2018-02-16 11:12:46 -0500

    renamed filters

--- A1-2015/SystemA/Plumber.java
+++ A1-2015/SystemA/Plumber.java
@@ -5,13 +5,10 @@
 * Copyright: Copyright (c) 2003 Carnegie Mellon University
 * Versions:
 *	1.0 November 2008 - Sample Pipe and Filter code (ajl).
+*   1.1 February 2017 - System A implementation.
 *
 * Description:
-*
-* This class serves as an example to illstrate how to use the PlumberTemplate to create a main thread that
-* instantiates and connects a set of filters. This example consists of three filters: a source, a middle filter
-* that acts as a pass-through filter (it does nothing to the data), and a sink filter which illustrates all kinds
-* of useful things that you can do with the input stream of data.
+* System A implementation.
 *
 * Parameters: 		None
 *
@@ -26,29 +23,29 @@ public class Plumber
 		* Here we instantiate three filters.
 		****************************************************************************/
 
-		SourceFilter Filter1 = new SourceFilter();
-		MiddleFilter1 Filter2 = new MiddleFilter1();
-	    MiddleFilter2 Filter3 = new MiddleFilter2();
-		SinkFilter Filter4 = new SinkFilter();
+		SourceFilter fileReaderSource = new SourceFilter();
+	    FahrenheitToCelsiusFilter fahrenheitToCelsius = new FahrenheitToCelsiusFilter();
+	    FeetToMetersFilter feetToMeters = new FeetToMetersFilter();
+		SinkFilter sink = new SinkFilter();
 
 		/****************************************************************************
-		* Here we connect the filters starting with the sink filter (Filter 1) which
-		* we connect to Filter2 the middle filter. Then we connect Filter2 to the
-		* source filter (Filter3).
+		* Here we connect the filters starting with the sink filter (sink) which
+		* we connect to feetToMeters the fahrenheitToCelsius filter. Then we connect fahrenheitToCelsius to the
+		* source filter (fileReaderSource).
 		****************************************************************************/
 
-	    Filter4.Connect(Filter3);
-		Filter3.Connect(Filter2); // This esstially says, "connect Filter3 input port to Filter2 output port
-		Filter2.Connect(Filter1); // This esstially says, "connect Filter2 intput port to Filter1 output port
+	    sink.Connect(feetToMeters);
+	    feetToMeters.Connect(fahrenheitToCelsius); // This esstially says, "connect Filter3 input port to Filter2 output port
+	    fahrenheitToCelsius.Connect(fileReaderSource); // This esstially says, "connect Filter2 intput port to Filter1 output port
 
 		/****************************************************************************
 		* Here we start the filters up. All-in-all,... its really kind of boring.
 		****************************************************************************/
 
-		Filter1.start();
-		Filter2.start();
-		Filter3.start();
-	    Filter4.start();
+	    fileReaderSource.start();
+	    fahrenheitToCelsius.start();
+		feetToMeters.start();
+	    sink.start();
 
    } // main
 

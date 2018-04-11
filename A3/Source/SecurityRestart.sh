8f4236fa5c189aa5dba60d746778e4aab870917c
Dario A Lencina Talarico <dario.talarico@getcruise.com>
2018-04-10 20:14:47 -0400

    bottom feeders

--- A3/Source/SecurityRestart.sh
+++ A3/Source/SecurityRestart.sh
@@ -6,10 +6,7 @@ sleep 2
 ECHO COMPILING SECURITY CLASSES
 export CLASS_PATH=.:SystemA/:SystemB/:SystemC/:InstrumentationPackage/
 
-javac *.java
-javac -cp ${CLASS_PATH} ./SystemA/*.java
-javac -cp ${CLASS_PATH} ./SystemB/*.java
-javac -cp ${CLASS_PATH} ./SystemC/*.java
+./Build.sh
 
 
 ./EMRestart.sh

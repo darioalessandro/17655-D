26ccc876867dd93333fd2fca05f257d17013a9a0
Dario A Lencina Talarico <dario.talarico@getcruise.com>
2018-04-14 14:49:48 -0400

    created base class for component

--- A3/Source/Shared/SecurityMonitor.java
+++ A3/Source/Shared/SecurityMonitor.java
@@ -6,48 +6,38 @@ import java.util.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import shared.*;
 
-class SecurityMonitor {
+class SecurityMonitor extends shared.Component {
     int SECURITY_MONITOR_CANCEL_SECURITY_ALARM = 10000;
     MessageQueue eq = null;               // Message Queue
     int MsgId = 0;                        // User specified message ID
-    int Delay = 2500;                    // The loop delay (2.5 seconds)
-    boolean Done = false;                // Loop termination flag
     Map<String,Boolean> alarmsState = new HashMap<>();
-    MessageManagerInterface em = null;
     boolean fireAlarmActive = false;
     boolean isAnyAlarmActive = false;
     boolean wasIntrusionAlertCancelled = false;
     Date alarmTransitionedToActive = new Date();
-
     MessageWindow mw = null;
     Indicator fireAlarmActiveIndicator;
     Indicator intrusionAlarmIndicator;
 
     SecurityMonitor(String args[]) throws Exception {
+        componentName = "Security Monitor";
         em = MessageManagerInterface.register(args);    // Interface object to the message manager
-    }
-
-    void sendHeartbeat(String name, long processId) throws Exception {
-        System.out.println("HEARTBEAT" + "." + name + "." + processId);
-        em.SendMessage(new Message( (int) 12, "HEARTBEAT" + "." + name + "." + processId));
-    }
-
-    void run() throws Exception {
         setupUI();
-        while (!Done) {
-            eq = em.GetMessageQueue();
-            for ( int i = 0; i < eq.GetSize(); i++ ) {
-                Message msg  = eq.GetMessage();
-                System.out.println(msg.GetMessage());
-                if ( msg.GetMessageId() == 10 ) {
-                    processMessage(msg);
-                }
+    }
+
+    @Override
+    public void run() throws Exception {
+        eq = em.GetMessageQueue();
+        for ( int i = 0; i < eq.GetSize(); i++ ) {
+            Message msg  = eq.GetMessage();
+            System.out.println(msg.GetMessage());
+            if ( msg.GetMessageId() == 10 ) {
+                processMessage(msg);
             }
-            processIntrusionAlarmsState();
-            sendHeartbeat("SecurityMonitor", em.ParticipantId);
-            Thread.sleep( Delay );
         }
+        processIntrusionAlarmsState();
     }
 
     void setupUI() {
@@ -108,6 +98,6 @@ class SecurityMonitor {
 
     public static void main(String args[]) throws Exception {
         SecurityMonitor monitor = new SecurityMonitor(args);
-        monitor.run();
+        monitor.start();
     }
 }
